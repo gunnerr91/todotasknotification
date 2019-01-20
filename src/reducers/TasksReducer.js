c3d16985afebf512647653028ea9658cac6e3caf
gunnerr91 <blissvortex@hotmail.com>
2019-01-20 17:59:18 +0000

    updated auth reducer and refactored actions

--- src/reducers/TasksReducer.js
+++ src/reducers/TasksReducer.js
@@ -1,7 +1,14 @@
+import { UPDATE_TASK } from "../actions/Types";
+
 const initial_state = {};
 
 export default (state = initial_state, action) => {
   switch (action.type) {
+    case UPDATE_TASK:
+      return {
+        ...state,
+        [action.payload.prop]: action.payload.value
+      };
     default:
       return {
         state
